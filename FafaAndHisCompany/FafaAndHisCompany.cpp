35cb29691601526f268920c261befa3629c56030
benedikt-hamberger <60503494+benedikt-hamberger@users.noreply.github.com>
2020-04-04 13:20:02 +0200

    Create FafaAndHisCompany.cpp

---
+++ FafaAndHisCompany/FafaAndHisCompany.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    int sum = 0;
+    for (int i = 1; i < n; ++i) {
+       if((n - i) % i == 0) sum++;
+    }
+
+    std::cout << sum;
+    return 0;
+}
